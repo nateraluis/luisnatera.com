ff81221ad41c2029b8b0bb390f436ec481d51679
Luis Natera <natera@hey.com>
2022-09-09 10:08:12 +0200

    Solve minor conflict

--- scripts/create_post.py
+++ scripts/create_post.py
@@ -1,13 +1,13 @@
+import datetime
 import os
 from collections import defaultdict
-import datetime
 
 from github import Github
 from rich import print
 
 
 def authenticated_github():
-    g = Github(os.environ['TOKEN_ISSUES'])
+    g = Github(os.environ["TOKEN_ISSUES"])
     return g
 
 
@@ -29,25 +29,31 @@ def get_content_from_issue(issue):
 def write_content_to_post(content):
     current_date = datetime.datetime.now().isoformat()[:10]
     with open(f"_posts/{current_date}-weekly.md", "w") as f:
-        f.write(f"---\ntitle: 'Week in Review {current_date}'\ndate: {current_date}\n"
-                f"permalink: /posts/{datetime.datetime.now().year}/{datetime.datetime.now().month}/{current_date}-review/\n"
-                "tags:\n  - weekly\n  - review\n  - automated\n"
-                "---\n")
-        for key in ['about', 'twitter', 'web']:
-            if f"#{key}" in content:
-                if key != 'about':
-                    f.write(f"## {key.capitalize()}\n")
-                for text in content[f"#{key}"]:
-                    if key != 'about':
-                        f.write(f"- {text}\n")
-                    else:
-                        f.write(f"{text}\n")
-                f.write("\n")
+        f.write(
+            f"---\ntitle: 'Week in Review {current_date}'\ndate: {current_date}\n"
+            f"permalink: /posts/{datetime.datetime.now().year}/{datetime.datetime.now().month}/{current_date}-review/\n"
+            "tags:\n  - weekly\n  - review\n  - automated\n"
+            "---\n"
+        )
+        tags = [k[1:] for k in content.keys()]
+        for key in tags:
+            print(f"✍️ Writing {key}")
+            if key != "about":
+                f.write(f"## {key.capitalize()}\n")
+            for text in content[f"#{key}"]:
+                if key != "about":
+                    f.write(f"- {text}\n")
+                else:
+                    f.write(f"{text}\n")
+            f.write("\n")
         f.write("***\n")
-        f.write("🤖 This post was generated automatically by the weekly script, using content curated in the "
-                "[issues](https://github.com/nateraluis/nateraluis.github.io/issues) "
-                "of my [repo](https://github.com/nateraluis/nateraluis.github.io/) in "
-                "[GitHub](https://github.com/nateraluis)\n")
+        f.write(
+            "🤖 This post was generated automatically by the weekly script, using content curated in the "
+            "[issues](https://github.com/nateraluis/nateraluis.github.io/issues) "
+            "of my [repo](https://github.com/nateraluis/nateraluis.github.io/) in "
+            "[GitHub](https://github.com/nateraluis)\n"
+        )
+        f.write("***\n")
         f.write("\n")
         f.write("***\n")
     print(f"✅ {current_date}-weekly.md post written.")
