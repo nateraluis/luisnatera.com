63819ba52e63185ece988c9931d0445ca4966c37
Luis Natera <natera@hey.com>
2024-01-31 19:26:44 +0100

    Remove unused variables

--- scripts/mastarchive.py
+++ scripts/mastarchive.py
@@ -6,7 +6,6 @@ from create_post import authenticated_github, get_issue
 
 
 def remove_html_tags(text):
-    """Remove html tags from a string"""
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
@@ -25,7 +24,6 @@ def write_favourites(issue, favourites):
         if status['created_at'] >= previous_date:
             content = remove_html_tags(status['content'])
             url = status['url'].strip()
-            date = status['created_at']
             text = f"#Mastodon [{content}]({url})"
             issue.create_comment(text)
 
